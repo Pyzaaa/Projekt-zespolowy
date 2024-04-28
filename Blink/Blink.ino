f53b9c66024564bfb848c098ded5b980e29c8412
Pyzaaa <bartekpyzara1@gmail.com>
2024-04-28 21:16:27 +0200

    Added custom library for the board (newlib), and code for testing both boards

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -31,7 +31,9 @@ void setup() {
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+    Serial.println("Blink on");
   delay(1000);                      // wait for a second
   digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+    Serial.println("Blink off");
   delay(1000);                      // wait for a second
 }
--- TESTBOTH-NEWLIB/TESTBOTH2/TESTBOTH2.ino
+++ TESTBOTH-NEWLIB/TESTBOTH2/TESTBOTH2.ino
@@ -1,5 +1,10 @@
+// kod do waveshare z examples/SMS
+// mamy zmodyfikowany kod z biblioteki Waveshare_SIM7600.cpp bez Powerkey bo powerkay nie działało
+
 #include <SPI.h>
 #include <mcp_can.h>
+
+#include "newlib.h" // do obslugi modulu SIM
 
 // Define the pins used for the CAN module
 const int SPI_CS_PIN = 10;
@@ -7,10 +12,18 @@ const int SPI_CS_PIN = 10;
 // Initialize MCP2515 object with CS pin
 MCP_CAN CAN(SPI_CS_PIN);
 
+char phone_number[] = "**********";      //********** change it to the phone number you want to call
+char text_message[] = "www.waveshare.com";      //
+
+
 void setup() {
   Serial.begin(9600);
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+
+
+  printf("Sending Short Message Test:\n");
+  sim7600.SendingShortMessage(phone_number,text_message);
 }
 
 void loop() {
